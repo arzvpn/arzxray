87818677d8167fd7869c294ce7303a8fd21814e6
arzvpn <121616001+arzvpn@users.noreply.github.com>
2023-01-05 20:45:04 +0700

    Update menu.sh

--- update/menu.sh
+++ update/menu.sh
@@ -220,9 +220,6 @@ echo -e "$COLOR1┌────────────────────�
 echo -e "$COLOR1│$NC Version     :${COLOR1} $(cat /opt/.ver) Latest Version${NC}"
 echo -e "$COLOR1│$NC Client Name :  ${GREEN}$Nama$NC"
 echo -e "$COLOR1│$NC License     : ${GREEN}$sisa_hari$NC Days Tersisa $NC"
-else
-    datediff "$Exp" "$DATE"
-fi;
 echo -e "$COLOR1└─────────────────────────────────────────────────┘$NC"
 echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
 echo -e "$COLOR1│${NC}             •Arz-VPN-STORE•              $COLOR1│$NC"

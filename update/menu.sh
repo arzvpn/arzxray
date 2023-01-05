30cbb634bfab6a2fa28b3c961b9704156dee34b9
arzvpn <121616001+arzvpn@users.noreply.github.com>
2023-01-05 20:01:43 +0700

    Update menu.sh

--- update/menu.sh
+++ update/menu.sh
@@ -35,7 +35,7 @@ export WARNING="${RED}\e[5m"
 export UNDERLINE="\e[4m"
 
 # // Exporting URL Host
-export Server_URL="raw.githubusercontent.com/arzvpn/proko/main"
+export Server_URL="raw.githubusercontent.com/arzvpn/prokontrol/main"
 export Server_Port="443"
 export Server_IP="underfined"
 export Script_Mode="Stable"

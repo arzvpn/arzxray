7bbd00edd172b814fd7ed667fb2dceeefe13015e
arzvpn <121616001+arzvpn@users.noreply.github.com>
2023-01-10 20:21:37 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -197,7 +197,7 @@ rm dependencies.sh
 clear
 
 echo "IP=" > /var/lib/arzpn-pro/ipvps.conf
-wget https://raw.githubusercontent.com/arzvpn/arzxray/main/cf.sh && chmod +x cf.sh && ./cf.sh
+wget https://raw.githubusercontent.com/arzvpn/arzxray/main/setdns.sh && chmod +x setdns.sh && ./setdns.sh
 rm -f /root/cf.sh
 
 #THEME RED
@@ -322,7 +322,7 @@ echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
 echo "   - IPtables                : [ON]"  | tee -a log-install.txt
 echo "   - Auto-Reboot             : [ON]"  | tee -a log-install.txt
 echo "   - IPv6                    : [OFF]"  | tee -a log-install.txt
-echo "   - Autoreboot On           : $aureb:00 $gg GMT +7" | tee -a log-install.txt
+echo "   - Autoreboot On           : 00.00 GMT +7" | tee -a log-install.txt
 echo "   - Autobackup Data" | tee -a log-install.txt
 echo "   - AutoKill Multi Login User" | tee -a log-install.txt
 echo "   - Auto Delete Expired Account" | tee -a log-install.txt

8c604cc5ea91e4ae0bc5cea65e864a8f664177ca
arzvpn <121616001+arzvpn@users.noreply.github.com>
2023-01-11 20:40:44 +0700

    Update menu.sh

--- update/menu.sh
+++ update/menu.sh
@@ -188,7 +188,7 @@ echo -e "  ${COLOR1}[4]${NC}  • TROJAN  [${YELLOW}Menu${NC}]   ${COLOR1}[10]${
 echo -e "  ${COLOR1}[5]${NC}  • SS WS   [${YELLOW}Menu${NC}]   ${COLOR1}[11]${NC} • SETTINGS [${YELLOW}Menu${NC}] $COLOR1❚$NC"
 echo -e "  ${COLOR1}[6]${NC}  • SET DNS [${YELLOW}Menu${NC}]   ${COLOR1}[12]${NC} • INFO     [${YELLOW}Menu${NC}] $COLOR1❚$NC"
 if [ "$Isadmin" = "ON" ]; then
-echo -e "  ${COLOR1}[15]${NC} • slowdns  [${YELLOW}Menu${NC}]
+echo -e " NC"
 echo -e "  ${COLOR1}[13]${NC} • REG IP  [${YELLOW}Menu${NC}]   ${COLOR1}[14]${NC} • SET BOT  [${YELLOW}Menu${NC}]  $COLOR1❚$NC"
 ressee="menu-ip"
 bottt="menu-bot"
@@ -246,7 +246,6 @@ case $opt in
 12) clear ; info ;;
 13) clear ; $ressee ;;
 14) clear ; $bottt ;;
-15) clear ; slowdnsmenu ;;
 100) clear ; $up2u ;;
 00 | 0) clear ; menu ;;
 *) clear ; menu ;;

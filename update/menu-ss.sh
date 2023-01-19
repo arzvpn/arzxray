109e364c072f67779379e092aa5f764237aa54b6
arzvpn <121616001+arzvpn@users.noreply.github.com>
2023-01-19 21:49:31 +0700

    Update menu-ss.sh

--- update/menu-ss.sh
+++ update/menu-ss.sh
@@ -557,7 +557,7 @@ echo -e " $COLOR1┌────────────────────
 echo -e " $COLOR1│$NC   ${COLOR1}[1]${NC} • ADD SHADOWSOCKS WS $NC"
 echo -e " $COLOR1│$NC   ${COLOR1}[2]${NC} • RENEW SHADOWSOCKS WS $NC"
 echo -e " $COLOR1│$NC   ${COLOR1}[3]${NC} • DELETE SHADOWSOCKS WS $NC" 
-echo -e " $COLOR1│$NC   ${COLOR1}[4]${NC} • USER ACTIVE $NC" 
+echo -e " $COLOR1│$NC   ${COLOR1}[4]${NC} • CEK USER ACTIVE $NC" 
 echo -e " $COLOR1│$NC   ${COLOR1}[0]${NC} • BACK TO MENU $NC"
 echo -e " $COLOR1└───────────────────────────────────────────────┘${NC}"
 echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"

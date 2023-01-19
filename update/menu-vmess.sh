b8f155924645013c81a3c6492f99defaf4556c55
arzvpn <121616001+arzvpn@users.noreply.github.com>
2023-01-19 21:15:58 +0700

    Update menu-vmess.sh

--- update/menu-vmess.sh
+++ update/menu-vmess.sh
@@ -389,7 +389,7 @@ echo -e " $COLOR1┌────────────────────
 echo -e " $COLOR1│$NC   ${COLOR1}[1]${NC} • ADD VMESS $NC"
 echo -e " $COLOR1│$NC   ${COLOR1}[2]${NC} • RENEW VMESS $NC"
 echo -e " $COLOR1│$NC   ${COLOR1}[3]${NC} • DELETE VMESS $NC"
-echo -e " $COLOR1│$NC   ${COLOR1}[4]${NC} • USER ACTIVE $NC"
+echo -e " $COLOR1│$NC   ${COLOR1}[4]${NC} • CEK USER ACTIVE $NC"
 echo -e " $COLOR1│$NC   ${COLOR1}[0]${NC} • BACK TO MENU $NC"
 echo -e " $COLOR1└───────────────────────────────────────────────┘${NC}"
 echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"

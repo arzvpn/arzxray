7607370e3e01d44849925fcea3db388b111aef85
arzvpn <121616001+arzvpn@users.noreply.github.com>
2023-01-19 21:18:24 +0700

    Update menu-vless.sh

--- update/menu-vless.sh
+++ update/menu-vless.sh
@@ -73,7 +73,7 @@ clear
 echo -n > /tmp/other.txt
 data=( `cat /etc/xray/config.json | grep '#&' | cut -d ' ' -f 2 | sort | uniq`);
 echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
-echo -e "$COLOR1│${NC} ${COLBG1}             •RENEW VLESS USER•              ${NC} $COLOR1│$NC"
+echo -e "$COLOR1│${NC} ${COLBG1}             •VLESS USER ACTIVE•              ${NC} $COLOR1│$NC"
 echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
 echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
 

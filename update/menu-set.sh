c61c0dd3617051d15bf20e00a9cbad8ada4fe11d
arzvpn <121616001+arzvpn@users.noreply.github.com>
2023-01-22 21:56:29 +0700

    Update menu-set.sh

--- update/menu-set.sh
+++ update/menu-set.sh
@@ -88,66 +88,66 @@ fi
 
 ssh=$(service ssh status | grep active | cut -d ' ' $stat)
 if [ "$ssh" = "active" ]; then
-ressh="${green}ONLINE${NC}"
+ressh="${green}RUNNING${NC}"
 else
-ressh="${red}OFFLINE${NC}"
+ressh="${red}NOT RUNNING${NC}"
 fi
 sshstunel=$(service stunnel4 status | grep active | cut -d ' ' $stat)
 if [ "$sshstunel" = "active" ]; then
-resst="${green}ONLINE${NC}"
+resst="${green}RUNNING${NC}"
 else
-resst="${red}OFFLINE${NC}"
+resst="${red}NOT RUNNING${NC}"
 fi
 sshws=$(service ws-dropbear status | grep active | cut -d ' ' $stat)
 if [ "$sshws" = "active" ]; then
-rews="${green}ONLINE${NC}"
+rews="${green}RUNNING${NC}"
 else
-rews="${red}OFFLINE${NC}"
+rews="${red}NOT RUNNING${NC}"
 fi
 
 sshws2=$(service ws-stunnel status | grep active | cut -d ' ' $stat)
 if [ "$sshws2" = "active" ]; then
-rews2="${green}ONLINE${NC}"
+rews2="${green}RUNNING${NC}"
 else
-rews2="${red}OFFLINE${NC}"
+rews2="${red}NOT RUNNING${NC}"
 fi
 
 db=$(service dropbear status | grep active | cut -d ' ' $stat)
 if [ "$db" = "active" ]; then
-resdb="${green}ONLINE${NC}"
+resdb="${green}RUNNING${NC}"
 else
-resdb="${red}OFFLINE${NC}"
+resdb="${red}NOT RUNNING${NC}"
 fi
  
 v2r=$(service $rekk status | grep active | cut -d ' ' $stat)
 if [ "$v2r" = "active" ]; then
-resv2r="${green}ONLINE${NC}"
+resv2r="${green}RUNNING${NC}"
 else
-resv2r="${red}OFFLINE${NC}"
+resv2r="${red}NOT RUNNING${NC}"
 fi
 vles=$(service $rekk status | grep active | cut -d ' ' $stat)
 if [ "$vles" = "active" ]; then
-resvles="${green}ONLINE${NC}"
+resvles="${green}RUNNING${NC}"
 else
-resvles="${red}OFFLINE${NC}"
+resvles="${red}NOT RUNNING${NC}"
 fi
 trj=$(service $rekk status | grep active | cut -d ' ' $stat)
 if [ "$trj" = "active" ]; then
-restr="${green}ONLINE${NC}"
+restr="${green}RUNNING${NC}"
 else
-restr="${red}OFFLINE${NC}"
+restr="${red}NOT RUNNING${NC}"
 fi
 
 ningx=$(service nginx status | grep active | cut -d ' ' $stat)
 if [ "$ningx" = "active" ]; then
-resnx="${green}ONLINE${NC}"
+resnx="${green}RUNNING${NC}"
 else
-resnx="${red}OFFLINE${NC}"
+resnx="${red}NOT RUNNING${NC}"
 fi
 
 squid=$(service squid status | grep active | cut -d ' ' $stat)
 if [ "$squid" = "active" ]; then
-ressq="${green}ONLINE${NC}"
+ressq="${green}RUNNING${NC}"
 else
 ressq="${red}OFFLINE${NC}"
 fi
@@ -164,7 +164,7 @@ echo -e " $COLOR1│${NC}  • WS STUNNEL                       • $rews2"
 echo -e " $COLOR1│${NC}  • STUNNEL                          • $resst"
 echo -e " $COLOR1│${NC}  • XRAY-SS                          • $resv2r"
 echo -e " $COLOR1│${NC}  • XRAY                             • $resv2r"
-echo -e " $COLOR1│${NC}  • VMESS                             • $resv2r"
+echo -e " $COLOR1│${NC}  • VMESS                            • $resv2r"
 echo -e " $COLOR1│${NC}  • VLESS                            • $resvles"
 echo -e " $COLOR1│${NC}  • TROJAN                           • $restr"
 echo -e " $COLOR1└───────────────────────────────────────────────┘${NC}" 

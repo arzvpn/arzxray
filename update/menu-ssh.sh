f77b57a26887358e287d2f7a4e51cad3c4522949
arzvpn <121616001+arzvpn@users.noreply.github.com>
2023-01-22 17:39:20 +0700

    Update menu-ssh.sh

--- update/menu-ssh.sh
+++ update/menu-ssh.sh
@@ -634,8 +634,8 @@ echo -e " $COLOR1┌────────────────────
  $COLOR1│$NC   ${COLOR1}[5]${NC} • DELETE SSH(${COLOR1}delssh${NC})
  $COLOR1│$NC   ${COLOR1}[6]${NC} • RENEW SSH(${COLOR1}renewssh${NC})
  $COLOR1│$NC   ${COLOR1}[7]${NC} • MEMBER SSH(${COLOR1}memberssh${NC})
- $COLOR1│$NC   ${COLOR1}[7]${NC} • SETUP AUTOKILL SSH(${COLOR1}autokill${NC})
- $COLOR1│$NC   ${COLOR1}[7]${NC} • CHECK USERS WHO DO MULTI LOGIN SSH(${COLOR1}ceklim${NC})
+ $COLOR1│$NC   ${COLOR1}[8]${NC} • SETUP AUTOKILL SSH(${COLOR1}autokill${NC})
+ $COLOR1│$NC   ${COLOR1}[9]${NC} • CHECK USERS WHO DO MULTI LOGIN SSH(${COLOR1}ceklim${NC})
  $COLOR1│$NC   ${COLOR1}[0]${NC} • BACK TO MENU"    
 echo -e " $COLOR1└───────────────────────────────────────────────┘${NC}" 
 echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"

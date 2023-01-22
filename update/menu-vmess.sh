712bf0848c4db705f4d3d2f00ecec7d90d900568
arzvpn <121616001+arzvpn@users.noreply.github.com>
2023-01-22 22:02:31 +0700

    Update menu-vmess.sh

--- update/menu-vmess.sh
+++ update/menu-vmess.sh
@@ -91,7 +91,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
 		echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
         echo "     No  Expired   User"
         grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | nl -s ') '
-        until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
+        until [[ ${CLIENT_NUMBER} -ge 0 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
                 if [[ ${CLIENT_NUMBER} == '0' ]]; then
                         read -rp "Select one client [1]: " CLIENT_NUMBER
                 else

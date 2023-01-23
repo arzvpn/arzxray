afdc8051b78e07f357c20ae0c35211bc2c99da93
arzvpn <121616001+arzvpn@users.noreply.github.com>
2023-01-23 13:23:59 +0700

    Update menu-backup.sh

--- update/menu-backup.sh
+++ update/menu-backup.sh
@@ -1,93 +1,56 @@
-#!/bin/bash
-dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-###########- COLOR CODE -##############
-colornow=$(cat /etc/arzvpn/theme/color.conf)
-NC="\e[0m"
-RED="\033[0;31m" 
-COLOR1="$(cat /etc/arzvpn/theme/$colornow | grep -w "TEXT" | cut -d: -f2|sed 's/ //g')"
-COLBG1="$(cat /etc/arzvpn/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g')"                    
-###########- END COLOR CODE -##########
+# SCRIPT BY Arz
 
-BURIQ () {
-    curl -sS https://raw.githubusercontent.com/arzvpn/permission/main/ipmini > /root/tmp
-    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
-    for user in "${data[@]}"
-    do
-    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
-    d1=(`date -d "$exp" +%s`)
-    d2=(`date -d "$biji" +%s`)
-    exp2=$(( (d1 - d2) / 86400 ))
-    if [[ "$exp2" -le "0" ]]; then
-    echo $user > /etc/.$user.ini
-    else
-    rm -f /etc/.$user.ini > /dev/null 2>&1
-    fi
-    done
-    rm -f /root/tmp
-}
-
-MYIP=$(curl -sS ipv4.icanhazip.com)
-Name=$(curl -sS https://raw.githubusercontent.com/arzvpn/permission/main/ipmini | grep $MYIP | awk '{print $2}')
-echo $Name > /usr/local/etc/.$Name.ini
-CekOne=$(cat /usr/local/etc/.$Name.ini)
-
-Bloman () {
-if [ -f "/etc/.$Name.ini" ]; then
-CekTwo=$(cat /etc/.$Name.ini)
-    if [ "$CekOne" = "$CekTwo" ]; then
-        res="Expired"
-    fi
-else
-res="Permission Accepted..."
-fi
-}
-
-PERMISSION () {
-    MYIP=$(curl -sS ipv4.icanhazip.com)
-    IZIN=$(curl -sS https://raw.githubusercontent.com/arzvpn/permission/main/ipmini | awk '{print $4}' | grep $MYIP)
-    if [ "$MYIP" = "$IZIN" ]; then
-    Bloman
-    else
-    res="Permission Denied!"
-    fi
-    BURIQ
-}
 red='\e[1;31m'
-green='\e[1;32m'
+green='\e[0;32m'
+purple='\e[0;35m'
+orange='\e[0;33m'
 NC='\e[0m'
-green() { echo -e "\\033[32;1m${*}\\033[0m"; }
-red() { echo -e "\\033[31;1m${*}\\033[0m"; }
-PERMISSION
-if [ -f /home/needupdate ]; then
-red "Your script need to update first !"
-exit 0
-elif [ "$res" = "Permission Accepted..." ]; then
-echo -ne
-else
-red "Permission Denied!"
+clear
+IP=$(wget -qO- icanhazip.com);
+date=$(date +"%Y-%m-%d")
+clear
+echo " VPS Data Backup By wunuit "
+sleep 1
+echo ""
+echo -e "[ ${green}INFO${NC} ] Please Insert Password To Secure Backup Data ."
+echo ""
+read -rp "Enter password : " -e InputPass
+clear
+sleep 1
+if [[ -z $InputPass ]]; then
 exit 0
 fi
+echo -e "[ ${green}INFO${NC} ] Processing . . . "
+mkdir -p /root/backup
+sleep 1
 clear
-echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
-echo -e "$COLOR1│${NC} ${COLBG1}             • BACKUP MENU •             ${NC} $COLOR1│$NC"
-echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
-echo -e " $COLOR1┌───────────────────────────────────────────────┐${NC}"
-echo -e " $COLOR1│$NC   ${COLOR1}[1]${NC} • BACKUP VPS $NC"
-echo -e " $COLOR1│$NC   ${COLOR1}[2]${NC} • RESTORE VPS $NC"                                             "
-echo -e " $COLOR1│$NC   ${COLOR1}[0]${NC} • BACK TO MENU $NC"
-echo -e " $COLOR1└───────────────────────────────────────────────┘${NC}"
-echo -e "$COLOR1┌──────────────────────────────────────────────────┐${NC}"
-echo -e "$COLOR1│${NC}             •Arz-VPN-STORE•              $COLOR1│$NC"
-echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
-echo -e ""
-read -p " Select menu :  "  opt
-echo -e ""
-case $opt in
-01 | 1) clear ; backup ;;
-02 | 2) clear ; restore ;;
-00 | 0) clear ; menu ;;
-*) clear ; menu-backup ;;
-esac
-
-       
+echo " Please Wait VPS Data Backup In Progress . . . "
+#cp -r /root/.acme.sh /root/backup/ &> /dev/null
+#cp -r /var/lib/premium-script/ /root/backup/premium-script
+#cp -r /etc/xray /root/backup/xray
+cp -r /etc/xray/*.json /root/backup/ >/dev/null 2>&1
+cp -r /home/vps/public_html /root/backup/public_html
+cp -r /etc/cron.d /root/backup/cron.d &> /dev/null
+cp -r /etc/crontab /root/backup/crontab &> /dev/null
+cd /root
+zip -rP $InputPass $IP-$date.zip backup > /dev/null 2>&1
+rclone copy /root/$IP-$date.zip dr:backup/
+url=$(rclone link dr:backup/$IP-$date.zip)
+id=(`echo $url | grep '^https' | cut -d'=' -f2`)
+link="https://drive.google.com/u/4/uc?id=${id}&export=download"
+clear
+echo -e "\033[1;37mVPS Data Backup By Arz\033[0m
+\033[1;37mTelegram : https://t.me/Store_Arz / @Store_Arz\033[0m"
+echo ""
+echo "Please Copy Link Below & Save In Notepad"
+echo ""
+echo -e "Your VPS IP ( \033[1;37m$IP\033[0m )"
+echo ""
+echo -e "Your VPS Data Backup Password : \033[1;37m$InputPass\033[0m"
+echo ""
+echo -e "\033[1;37m$link\033[0m"
+echo ""
+echo "If you want to restore data, please enter the link above"
+rm -rf /root/backup
+rm -r /root/$IP-$date.zip
+echo ""

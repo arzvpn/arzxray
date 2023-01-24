52e188f4d1a3f2de52b364c57264116c1b401e03
arzvpn <121616001+arzvpn@users.noreply.github.com>
2023-01-24 19:10:23 +0700

    Update about.sh

--- update/about.sh
+++ update/about.sh
@@ -1,1 +1,25 @@
+          *               **************            *********************
+         *  *             *              *                           *
+        *    *            *               *                        *
+       *      *           *             *                        *
+      *        *          **************                       *
+     *          *         *             *                    *
+    **************        *              *                 *
+   *              *       *               *              *
+  *                *      *                *           *
+ *                  *     *                 *        *
+*                    *    *                  *    *********************
 
+
+>>>>>>>>>>Welcome to AutoScript Premium V1<<<<<<<<<<
+>>>>>>>>>>>>>>>>>>AUTOSCRIPT BY Arz<<<<<<<<<<<<<<<<<
+
+>Support OS 
+For Ubuntu 18 & Ubuntu 20 
+For Debian 9 & Debian 10
+
+Contact Owner AutoScript
+> WA: 083117634078
+> TELEGRAM: t.me/Store_Arz
+
+Thanks for your Order Arz AutoScript

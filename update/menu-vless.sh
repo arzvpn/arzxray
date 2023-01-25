376829c90e48fe2c4ff05ca0bbe08df132bb380b
arzvpn <121616001+arzvpn@users.noreply.github.com>
2023-01-25 21:42:25 +0700

    Update menu-vless.sh

--- update/menu-vless.sh
+++ update/menu-vless.sh
@@ -412,13 +412,14 @@ menu-vless
 
 clear
 echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
-echo -e "$COLOR1│${NC} ${COLBG1}             •VLESS MENU•              ${NC} $COLOR1│$NC"
+echo -e "$COLOR1│${NC} ${COLBG1}               •VLESS MENU•                ${NC} $COLOR1│$NC"
 echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
 echo -e " $COLOR1┌───────────────────────────────────────────────┐${NC}"
 echo -e " $COLOR1│$NC   ${COLOR1}[1]${NC} • CREATE VLESS ACCOUNT $NC"
 echo -e " $COLOR1│$NC   ${COLOR1}[2]${NC} • TRIAL VLESS $NC"
 echo -e " $COLOR1│$NC   ${COLOR1}[3]${NC} • RENEW VLESS $NC"
-echo -e " $COLOR1│$NC   ${COLOR1}[5]${NC} • CHECK USER ACTIVE $NC
+echo -e " $COLOR1│$NC   ${COLOR1}[4]${NC} • DELETE VLESS $NC"
+echo -e " $COLOR1│$NC   ${COLOR1}[5]${NC} • CHECK USER ACTIVE $NC"
 echo -e " $COLOR1│$NC   ${COLOR1}[6]${NC} • DETAIL VLESS $NC"
 echo -e " $COLOR1│$NC   ${COLOR1}[0]${NC} • BACK TO MENU $NC"
 echo -e " $COLOR1└───────────────────────────────────────────────┘${NC}"

075bb11eefc337aa34e3f529f306189c17457c00
arzvpn <121616001+arzvpn@users.noreply.github.com>
2023-03-10 17:58:56 +0700

    Update openssh-socket.py

--- websocket/openssh-socket.py
+++ websocket/openssh-socket.py
@@ -12,7 +12,7 @@ PASS = ''
 BUFLEN = 4096 * 4
 TIMEOUT = 60
 DEFAULT_HOST = '127.0.0.1:88'
-RESPONSE = 'HTTP/1.1 101 <font color="cyan"><b><i>AUTOSCRIPT-Arz-VPN</font></b></i>\r\nContent-Length: 104857600000\r\n\r\n'
+RESPONSE = 'HTTP/1.1 101 <font color="lime"><b><i>AutoScript Arz</font></b></i>\r\nContent-Length: 104857600000\r\n\r\n'
 
 class Server(threading.Thread):
     def __init__(self, host, port):

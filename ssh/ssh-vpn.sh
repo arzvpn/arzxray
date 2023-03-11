ffeffd32610131f4370e8ea3b8fd9fde1867c155
arzvpn <121616001+arzvpn@users.noreply.github.com>
2023-03-12 06:47:35 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -181,10 +181,16 @@ echo "=== Install Dropbear ==="
 sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
 sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=143/g' /etc/default/dropbear
 sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 50000 -p 109 -p 110 -p 69"/g' /etc/default/dropbear
+systemctl daemon-reload >/dev/null 2>&1
+systemctl start dropbear >/dev/null 2>&1
+systemctl restart dropbear >/dev/null 2>&1
+cekker=$(cat /etc/shells | grep -w "/bin/false")
+if [[ "$cekker" = "/bin/false" ]];then
+echo -ne
+else
 echo "/bin/false" >> /etc/shells
 echo "/usr/sbin/nologin" >> /etc/shells
-/etc/init.d/ssh restart
-/etc/init.d/dropbear restart
+fi
 
 cd
 # install stunnel

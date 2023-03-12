cc281a10d14566192e2e1345a2bc124e0a3fb29c
arzvpn <121616001+arzvpn@users.noreply.github.com>
2023-03-12 07:45:12 +0700

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -7,6 +7,7 @@ NC='\e[0m'
 green() { echo -e "\\033[32;1m${*}\\033[0m"; }
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 
+clear
 if [[ -e /etc/debian_version ]]; then
 	source /etc/os-release
 	OS=$ID # debian or ubuntu
@@ -15,47 +16,11 @@ elif [[ -e /etc/centos-release ]]; then
 	OS=centos
 fi
 
-
-
-
-echo "Tools install...!"
+echo "Arz"
 echo "Progress..."
 sleep 2
 
-# // Remove
-apt-get remove --purge nginx* -y
-apt-get remove --purge nginx-common* -y
-apt-get remove --purge nginx-full* -y
-apt-get remove --purge dropbear* -y
-apt-get remove --purge stunnel4* -y
-apt-get remove --purge apache2* -y
-apt-get remove --purge ufw* -y
-apt-get remove --purge firewalld* -y
-apt-get remove --purge exim4* -y
-apt autoremove -y
-apt update -y
-# // Install Requirement Tools
-apt-get --reinstall --fix-missing install -y sudo dpkg psmisc socat jq ruby wondershaper python2 tmux nmap bzip2 gzip coreutils wget screen rsyslog iftop htop net-tools zip unzip wget vim net-tools curl nano sed screen gnupg gnupg1 bc apt-transport-https build-essential gcc g++ automake make autoconf perl m4 dos2unix dropbear libreadline-dev zlib1g-dev libssl-dev dirmngr libxml-parser-perl neofetch git lsof iptables iptables-persistent
-apt-get --reinstall --fix-missing install -y libreadline-dev zlib1g-dev libssl-dev python2 screen curl jq bzip2 gzip coreutils rsyslog iftop htop zip unzip net-tools sed gnupg gnupg1 bc sudo apt-transport-https build-essential dirmngr libxml-parser-perl neofetch screenfetch git lsof openssl easy-rsa fail2ban tmux vnstat dropbear libsqlite3-dev socat cron bash-completion ntpdate xz-utils sudo apt-transport-https gnupg2 gnupg1 dnsutils lsb-release chrony
-gem install lolcat
-apt update -y
-apt dist-upgrade -y
-apt install sudo -y
-
-
-apt install -y screen curl jq bzip2 gzip coreutils rsyslog iftop \
-htop zip unzip net-tools sed gnupg gnupg1 \
-bc sudo apt-transport-https build-essential dirmngr libxml-parser-perl neofetch screenfetch git lsof \
-openssl openvpn easy-rsa fail2ban tmux \
-stunnel4 vnstat squid3 \
-dropbear  libsqlite3-dev \
-socat cron bash-completion ntpdate xz-utils sudo apt-transport-https \
-gnupg2 dnsutils lsb-release chrony
-
-curl -sSL https://deb.nodesource.com/setup_16.x | bash - 
-apt-get install nodejs -y
-
-/etc/init.d/vnstat restart
+/etc/init.d/vnstat restart >/dev/null 2>&1
 wget -q https://humdi.net/vnstat/vnstat-2.6.tar.gz
 tar zxvf vnstat-2.6.tar.gz
 cd vnstat-2.6
@@ -64,17 +29,12 @@ cd
 vnstat -u -i $NET
 sed -i 's/Interface "'""eth0""'"/Interface "'""$NET""'"/g' /etc/vnstat.conf
 chown vnstat:vnstat /var/lib/vnstat -R
-systemctl enable vnstat
-/etc/init.d/vnstat restart
+systemctl enable vnstat >/dev/null 2>&1
+/etc/init.d/vnstat restart >/dev/null 2>&1
 rm -f /root/vnstat-2.6.tar.gz >/dev/null 2>&1
 rm -rf /root/vnstat-2.6 >/dev/null 2>&1
 
-
-fi
-
-apt install -y libnss3-dev libnspr4-dev pkg-config libpam0g-dev libcap-ng-dev libcap-ng-utils libselinux1-dev libcurl4-nss-dev flex bison make libnss3-tools libevent-dev xl2tpd pptpd
-
 yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
-yellow "Dependencies successfully installed..."
+yellow "Source successfully installed..."
 sleep 3
 clear

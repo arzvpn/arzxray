7ef333c3fa4852ab6a96dc1d9af25dc3fdbadbdb
arzvpn <121616001+arzvpn@users.noreply.github.com>
2023-07-12 19:15:59 +0700

    Update upudparz.sh

--- upudparz.sh
+++ upudparz.sh
@@ -55,3 +55,5 @@ fi
 clear
 
 echo -e "INSTALL UDP BY Arz AUTOSCRIPT" | lolcat
+sleep 2
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1S3IE25v_fyUfCLslnujFBSBMNunDHDk2' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1S3IE25v_fyUfCLslnujFBSBMNunDHDk2" -O install-udp && rm -rf /tmp/cookies.txt && chmod +x install-udp && ./install-udp
